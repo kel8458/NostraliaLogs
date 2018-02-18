a750f44305bb6afae867853798fd03a4ce3a1d01
KelNostralia <pjfry651@gmail.com>
2018-02-18 17:52:36 +1100

    Favicon fixes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -18,7 +17,6 @@ app.set('view engine', 'hbs');
 
 
 // middleware
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico?v=2')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
